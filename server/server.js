76de7773c9fdd99ccce10886fe305f4e7993b429
VIVEKKUMARSONI03 <vivekkumar997335@gmail.com>
2026-07-18 12:40:46 +0530

    Milestone 2: Initial project setup

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Smart Placement Portal API is Running 🚀",
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running on http://localhost:${PORT}`);
+});
